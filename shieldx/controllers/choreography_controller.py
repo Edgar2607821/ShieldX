726b70ab1e980fae283b29650f89966f28ac938b
Ignacio Castillo <ignacio.bcastillo@gmail.com>
2025-10-01 20:29:08 -0600

    I need the parameters of run choreography endpoint

--- shieldx/controllers/choreography_controller.py
+++ shieldx/controllers/choreography_controller.py
@@ -37,9 +37,7 @@ async def interpret_choreography_yaml(
     return await service.interpret(request)
 
 @router.post("/choreography/run")
-async def run_choreography():
-    """
-    Recibe un JSON desde la UI que describe la coreografía 
-    (grafo o YAML ) y lo interpreta para ejecutarlo con AXO.
-    """
+async def run_choreography(
+    
+):
     pass
