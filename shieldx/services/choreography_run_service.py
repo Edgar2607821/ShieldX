96c2126b294e4621f13f760b6e0ebffcedacf735
Edgar2607821 <edgarballeza87@gmail.com>
2025-10-03 14:30:15 -0600

    feat(choreography): add run service and controller endpoint to execute choreographies with AXO

---
+++ shieldx/services/choreography_run_service.py
@@ -0,0 +1,84 @@
+import networkx as nx
+from option import Result
+from axo.contextmanager import AxoContextManager
+from axo.endpoint.manager import DistributedEndpointManager
+from axo.storage.services import MictlanXStorageService
+from axo.errors import AxoError
+from axo import Axo
+
+from shieldx_core.dtos import GraphSpecDTO, NodeDTO
+
+class ChoreographyRunServiece:
+    def __init__(self):
+        self.endpoint_manager = DistributedEndpointManager()
+        self.storage_service = MictlanXStorageService()
+
+    async def run(self, graph: GraphSpecDTO):
+
+        dem = DistributedEndpointManager()
+        endpoint_ids = {
+            node.rule.target.axo_endpoint_id
+            for node in graph.vertices
+            if node.type == "ActiveObject" and node.rule
+        }
+        for eid in endpoint_ids:
+            dem.add_endpoint(
+                endpoint_id=eid,
+                hostname="localhost",
+                protocol="tcp",
+                req_res_port=16667,
+                pubsub_port=16666
+            )
+
+        ss = MictlanXStorageService()
+
+        G = nx.DiGraph()
+        for v in graph.vertices:
+            G.add_node(v.id, data=v)
+        for e in graph.edges:
+            G.add_edge(e.from_, e.to)
+
+        # 2. Orden topológico de ejecución
+        order = list(nx.topological_sort(G))
+        results = {}
+
+
+        with AxoContextManager.distributed(endpoint_manager=dem, storage_service=ss):
+            for node_id in order:
+                node: NodeDTO = G.nodes[node_id]["data"]
+
+                if node.type == "ActiveObject":
+
+                    res = await Axo.get_by_key(
+                                                bucket_id=node.rule.target.axo_bucket_id,
+                                                key=node.name
+                    )
+                    if not res.is_ok:
+                        raise Exception(f"No se pudo recuperar AO {node.name}")
+                    
+                    ao: Axo = res.unwrap()
+
+                    # Configurar en tiempo de ejecución
+                    ao.set_endpoint_id(node.rule.target.axo_endpoint_id)
+                    if node.params.init:
+                        ao.set_source_bucket_id(node.params.init.get("source_bucket_id"))
+                        ao.set_sink_bucket_id(node.params.init.get("sink_bucket_id"))
+
+                    # Ejecutar método
+                    method_name = node.rule.target.alias.split(".")[-1]
+                    call_params = node.params.call or {}
+                    res: Result = getattr(ao, method_name)(**call_params)
+
+                    if res.is_ok:
+                        ball_ref = res.unwrap()
+                        result = await ball_ref.to_pointer(
+                            self.storage_service,
+                            consume=True,
+                            delete_remote=False
+                        ).into_bytes()
+                        results[node.id] = result.unwrap()
+                    else:
+                        err: AxoError = res.unwrap_err()
+                        results[node.id] = f"Error: {err}"
+
+        return results
