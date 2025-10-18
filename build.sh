06400f814f33d54d4786308cc6ceb45e9460092e
Edgar2607821 <edgarballeza87@gmail.com>
2025-10-18 01:48:38 -0600

    feat(build): separate build logic from deployment and improve argument handling

--- build.sh
+++ build.sh
@@ -3,9 +3,10 @@ set -e  # Detener ejecución si ocurre algún error
 
 # Variables
 
-IMAGE_NAME=${1:-"edgar821/shieldx-api"}
+IMAGE_NAME=${1:-"shieldx:api"}
 IMAGE_TAG=${2:-"latest"}
-COMPOSE_FILE=${3:-"docker-compose.yml"}
+DOCKERFILE=${3:-"./Dockerfile"}
+BUILD_CONTEXT=${4:-"."}
 
 # Banner de inicio para ShieldX
 echo -e "\e[36m"  # Color cian
@@ -23,23 +24,28 @@ echo
 echo "=============================================="
 echo " 🛠️  Building ShieldX Docker image"
 echo "----------------------------------------------"
-echo " Imagen: $IMAGE_NAME:$IMAGE_TAG"
-echo " Compose file: $COMPOSE_FILE"
+echo " Image      : ${IMAGE_NAME}:${IMAGE_TAG}"
+echo " Dockerfile : ${DOCKERFILE}"
+echo " Context    : ${BUILD_CONTEXT}"
 echo "=============================================="
 
 # Construcción de la imagen local
+docker build -f "${DOCKERFILE}" -t "${IMAGE_NAME}:${IMAGE_TAG}" "${BUILD_CONTEXT}"
 
-docker build -f ./Dockerfile -t $IMAGE_NAME:$IMAGE_TAG .
+echo "✅ Image built successfully: ${IMAGE_NAME}:${IMAGE_TAG}"
 
-echo "✅ Image built successfully: $IMAGE_NAME:$IMAGE_TAG"
 
-if [ -f "$COMPOSE_FILE" ]; then
-    echo "📦 Starting stack with Docker Compose..."
-    docker compose -f "$COMPOSE_FILE" down || true
-    docker compose -f "$COMPOSE_FILE" up -d
-    echo "✅ Stack deployed successfully."
-else
-    echo "⚠️  $COMPOSE_FILE not found, only the image was built."
-fi
-
-echo "✅ Process completed successfully."
+#
+#echo "✅ Image built successfully: $IMAGE_NAME:$IMAGE_TAG"
+#
+#if [ -f "$COMPOSE_FILE" ]; then
+#    echo "📦 Starting stack with Docker Compose..."
+#    docker compose -f "$COMPOSE_FILE" down || true
+#    docker compose -f "$COMPOSE_FILE" up -d
+#    echo "✅ Stack deployed successfully."
+#else
+#    echo "⚠️  $COMPOSE_FILE not found, only the image was built."
+#fi
+#
+#echo "✅ Process completed successfully."
+#
